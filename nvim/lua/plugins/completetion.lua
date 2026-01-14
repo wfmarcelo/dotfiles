80c06447ff85e977a5d11f8842e66b3876df557c
Marcelo Wollmann Figueiró <marcelo.wollmann@uniarp.edu.br>
2026-01-14 07:59:32 -0300

    fix: database config

--- nvim/lua/plugins/completetion.lua
+++ nvim/lua/plugins/completetion.lua
@@ -1,26 +1,34 @@
 return {
   {
-    'Saghen/blink.cmp',
-    version = '*', -- Use latest stable release
+    "Saghen/blink.cmp",
+    version = "*", -- Use latest stable release
+    dependencies = { "kristijanhusak/vim-dadbod-completion" },
     opts = {
       -- 'default' for modular/standard setup
-      keymap = { preset = 'default' },
+      keymap = { preset = "default" },
 
       appearance = {
         use_nvim_cmp_as_default = true,
-        nerd_font_variant = 'mono'
+        nerd_font_variant = "mono",
       },
 
       -- Tell blink to use your LSP (Roslyn) as a source
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { "lsp", "path", "snippets", "buffer", "dadbod" },
+        providers = {
+          dadbod = {
+            name = "Dadbod",
+            module = "vim_dadbod_completion.blink",
+            score_offset = 100, -- Make dadbod suggestions appear higher than general buffer words
+          },
+        },
       },
 
       -- Modern UI for the completion menu
       completion = {
-        menu = { border = 'rounded' },
-        documentation = { window = { border = 'rounded' } },
+        menu = { border = "rounded" },
+        documentation = { window = { border = "rounded" } },
       },
     },
-  }
+  },
 }
--- nvim/lua/plugins/database.lua
+++ nvim/lua/plugins/database.lua
@@ -8,6 +8,14 @@ return {
     config = function()
       -- Optional: Configure where the UI appears
       vim.g.db_ui_save_location = vim.fn.stdpath("config") .. "/db_ui"
+
+      vim.g.db_ui_win_preference = "vertical" -- Opens queries in vertical splits
+      vim.g.db_ui_force_echo_notifications = 1 -- Shows errors in the command line if it fails
+      vim.g.db_ui_show_database_navigation = 1
+      -- vim.g.db_adapter_jdbc_mssql_jar = "/usr/local/lib/jdbc/mssql-jdbc.jar"
+
+      vim.keymap.set("n", "<leader>S", "<Plug>(DBUI_ExecuteQuery)", { desc = "Execute SQL Query" })
+      vim.keymap.set("v", "<leader>S", "<Plug>(DBUI_ExecuteQuery)", { desc = "Execute Selection" })
     end,
   },
 }
